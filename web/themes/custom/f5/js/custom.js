4bc00b32f74cfc7f1841270763a4a8f70696483d
pagach <alen.pagac@gmail.com>
2019-09-15 11:09:31 +0200

    Added slick and prepared js libraries.

---
+++ web/themes/custom/f5/js/custom.js
@@ -0,0 +1,7 @@
+(function ($, Drupal, drupalSettings) {
+  'use strict';
+  Drupal.behaviors.argenta = {
+    attach: function (context, settings) {
+    }
+  };
+})(jQuery, Drupal, drupalSettings);
